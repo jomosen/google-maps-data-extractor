8d9fafc2d56534884513857ce8342013b1e8a6b8
jomosen <josemorellsendra@gmail.com>
2026-02-19 18:13:18 +0100

    Remove GeonameSelectionService and campaign-related enums for code cleanup

--- src/extraction/domain/enums/campaign_depth_level.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class CampaignDepthLevel(str, Enum):
-    ADMIN1 = "admin1"
-    ADMIN2 = "admin2"
-    ADMIN3 = "admin3"
-    CITY = "city"
--- src/extraction/domain/enums/campaign_scope.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class CampaignScope(str, Enum):
-    COUNTRY = "country"
-    ADMIN1 = "admin1"
-    ADMIN2 = "admin2"
-    CITY = "city"
