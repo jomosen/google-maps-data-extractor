b2f548cc88932fd28352c51141ef0e4c04c0a9f4
jomosen <josemorellsendra@gmail.com>
2026-02-19 18:14:46 +0100

    Add conditional rendering for CampaignDetailView in App component

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,6 +3,7 @@ import { useAppStore } from './store/appStore';
 import { DashboardView } from './presentation/views/DashboardView';
 import { CreateCampaignView } from './presentation/views/CreateCampaignView';
 import { MonitorView } from './presentation/views/MonitorView';
+import { CampaignDetailView } from './presentation/views/CampaignDetailView';
 
 function App() {
   const currentView = useAppStore((state) => state.currentView);
@@ -12,6 +13,7 @@ function App() {
       {currentView === 'dashboard' && <DashboardView />}
       {currentView === 'create' && <CreateCampaignView />}
       {currentView === 'monitor' && <MonitorView />}
+      {currentView === 'detail' && <CampaignDetailView />}
     </div>
   );
 }
